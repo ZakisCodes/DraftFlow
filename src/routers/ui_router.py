7e9402819e4aa356f03cf131ac13dbbcaed4e028
ZakisCodes <zakinabdul.tech@gmail.com>
2025-06-03 03:24:09 +0000

    Added timestamp context to template responses for development purposes

--- src/routers/ui_router.py
+++ src/routers/ui_router.py
@@ -1,17 +1,29 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-
+from datetime import datetime
 router = APIRouter()
 template = Jinja2Templates(directory="templates")
 
+
+# added time just for development purpose
+# "time" : datetime.utcnow().timestamp()
+
 @router.get("/home")
 async def home(req: Request):
-    return template.TemplateResponse(name="home.html", context={"request": req})
+    return template.TemplateResponse(name="home.html", 
+                                     context={
+                                         "request": req,
+                                         "time" : datetime.utcnow().timestamp()})
 
 @router.get("/editor")
 async def result(req: Request):
-    return template.TemplateResponse(name="editor.html", context={"request": req})
+    return template.TemplateResponse(name="editor.html", context={
+                                        "request": req,
+                                        "time" : datetime.utcnow().timestamp()})
 
 @router.get("/")
 async def auth(req: Request):
-    return template.TemplateResponse(name="auth.html", context={"request": req})
+    return template.TemplateResponse(name="auth.html", context={
+                                        "request": req,
+                                         "time" : datetime.utcnow().timestamp()})
+
