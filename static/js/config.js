6efc1d4d0766a896ea49eaf5e8f11202acc626eb
ZakisCodes <zakinabdul.tech@gmail.com>
2025-06-12 16:53:43 +0000

    Add Firebase configuration file for project setup

---
+++ static/js/config.js
@@ -0,0 +1,11 @@
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBp5hN8nhz-pYkyo6MZvysR4ViB1ehWfJ0",
+  authDomain: "draftflow-b7b11.firebaseapp.com",
+  projectId: "draftflow-b7b11",
+  storageBucket: "draftflow-b7b11.firebasestorage.app",
+  messagingSenderId: "269160881503",
+  appId: "1:269160881503:web:b8dd4446dbd68bbc626aea",
+  measurementId: "G-HTEHKFJ880",
+};
